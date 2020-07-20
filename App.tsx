3998355b270717071bde6555e0c40b679ea0f3c1
Mateus Paixão <mateus.paixao@corebiz.com.br>
2020-07-20 20:58:03 -0300

    props and icons, add scrollview for logos and courses

--- App.tsx
+++ App.tsx
@@ -1,17 +1,52 @@
 import React from "react";
 import styled from "styled-components/native";
 import Card from "./components/Card";
+import { ScrollView, SafeAreaView } from 'react-native'
+import { NotificationIcon } from './components/Icons'
+import Logo from "./components/Logo";
+
 
 export default function App() {
   return (
     <Container>
-      <TitleBar>
-        <Avatar source={require("./assets/avatar.jpg")} />
-        <Title>Welcome back,</Title>
-        <Name>Meng</Name>
-      </TitleBar>
-      <Subtitle>Continue learning</Subtitle>
-      <Card></Card>
+      <SafeAreaView>
+      <ScrollView>
+        <TitleBar>
+          <Avatar source={require("./assets/avatar.jpg")} />
+          <Title>Welcome back,</Title>
+          <Name>Meng</Name>
+          <NotificationIcon style={{ position: "absolute", right: 20, top: 5 }}/>
+        </TitleBar>
+
+        <ScrollView style={{ flexDirection: "row", padding: 20, paddingLeft: 12, paddingTop: 30, height: 120 }} horizontal={true}>
+          <Logo image={require('./assets/logo-framerx.png')} text="Framer X"/>
+          <Logo image={require('./assets/logo-figma.png')} text="Figma"/>
+        </ScrollView>
+
+        <Subtitle>Continue learning</Subtitle>
+
+        <ScrollView
+          style={{ paddingBottom: 30 }}
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
+        >
+          <Card 
+            title="Styled Components"
+            image={require('./assets/background2.jpg')}
+            caption="React Native"
+            logo={require('./assets/logo-react.png')}
+            subtitle="5 of 12 sections"
+          />
+          <Card 
+            title="Styled Components"
+            image={require('./assets/background2.jpg')}
+            caption="React Native"
+            logo={require('./assets/logo-react.png')}
+            subtitle="5 of 12 sections"
+          />
+        </ScrollView>
+      </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 }
@@ -21,7 +56,7 @@ const Subtitle = styled.Text`
   font-weight: 600;
   font-size: 15px;
   margin-left: 20px;
-  margin-top: 20px;
+  /* margin-top: 20px; */
   text-transform: uppercase;
 `;
 
